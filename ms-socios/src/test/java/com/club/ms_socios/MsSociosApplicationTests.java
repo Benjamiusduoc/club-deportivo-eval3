8a8d5b46d2e08a357821d9381720acdb0a22726b
Benjamin Salgado <be.salgadol@duocuc.cl>
2026-06-08 22:39:16 -0400

    test(ms-socios): add Spring Boot context load smoke test

---
+++ ms-socios/src/test/java/com/club/ms_socios/MsSociosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.ms_socios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSociosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
