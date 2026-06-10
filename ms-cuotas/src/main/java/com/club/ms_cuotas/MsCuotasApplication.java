123954ee9e54d228b44a5bcc32d586a5b4fa8cea
Benjamin Salgado <be.salgadol@duocuc.cl>
2026-06-09 20:40:44 -0400

    chore(ms-cuotas): scaffold project structure with pom, Dockerfile, OpenAPI config

---
+++ ms-cuotas/src/main/java/com/club/ms_cuotas/MsCuotasApplication.java
@@ -0,0 +1,12 @@
+package com.club.ms_cuotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCuotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCuotasApplication.class, args);
+	}
+}
