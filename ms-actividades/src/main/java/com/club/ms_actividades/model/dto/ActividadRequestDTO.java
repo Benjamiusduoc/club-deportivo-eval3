0d8d59db86c5358217621b8153eaae4cf1e85f86
Brahyans2 <brah.rojas@duocuc.cl>
2026-06-13 19:57:41 -0400

    Update Agrego validaciones de tamaño en DTO

--- ms-actividades/src/main/java/com/club/ms_actividades/model/dto/ActividadRequestDTO.java
+++ ms-actividades/src/main/java/com/club/ms_actividades/model/dto/ActividadRequestDTO.java
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import jakarta.validation.constraints.Size;
+    import lombok.Data;
 
 import java.math.BigDecimal;
 
@@ -14,13 +15,14 @@ public class ActividadRequestDTO {
     @NotBlank(message = "El nombre no puede estar vacío")
     private String nombre;
 
+    @Size(max = 100, message = "La descripción es muy larga")
     private String descripcion;
 
-    @NotNull(message = "El cupo máximo es obligatorio")
+    @NotNull(message = "Debe indicar el cupo máximo de la actividad")
     @Min(value = 1, message = "El cupo máximo debe ser al menos 1")
     private Integer cupoMaximo;
 
-    @NotNull(message = "El precio es obligatorio")
+    @NotNull(message = "Debe indicar el precio de la actividad")
     @DecimalMin(value = "0.01", message = "El precio debe ser mayor a 0")
     private BigDecimal precio;
 }
